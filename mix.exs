1938cfa2d6194d6c15b20b254230b1901f2737e3
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2017-01-10 16:24:54 +0100

    bump version, allow 1.3

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule DelayedOTP.Mixfile do
 
   def project do
     [app: :delayed_otp,
-     version: "0.0.2",
-     elixir: "~> 1.2",
+     version: "0.0.3",
+     elixir: ">= 1.2.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      package: [
