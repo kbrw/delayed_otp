70171ad269c0db93998f4560c4e182a89779abda
DarkyZ aka NotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2024-02-05 16:22:39 +0100

    Bump project version in mix.exs

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DelayedOTP.Mixfile do
 
   def project do
     [app: :delayed_otp,
-     version: "0.0.4",
+     version: "0.1.0",
      elixir: ">= 1.11.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
